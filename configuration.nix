4a0f1e76e485ff2ff09425c553927235c1e3c234
Jonas <jonas.bonnaudet@gmail.com>
2025-02-04 02:25:18 +0100

    zsh

--- configuration.nix
+++ configuration.nix
@@ -109,6 +109,25 @@
     localNetworkGameTransfers.openFirewall = true;
   };
 
+  # zsh
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    autosuggestions.enable = true;
+    syntaxHighlighting.enable = true;
+
+    shellAliases = {
+      ll = "ls -l";
+      update = "sudo nixos-rebuild switch";
+    };
+    oh-my-zsh = {
+        enable = true;
+        plugins = [ "git" ];
+        theme = "robbyrussell";
+      };
+
+  };
+
   # sway
   programs.sway = {
   	enable = true;
