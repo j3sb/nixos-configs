c1b17f49d43fb9050312363ad4ecf27c6d07a50f
Jonas <jonas.bonnaudet@gmail.com>
2025-07-17 11:41:34 +0200

    nixos unstable

--- common.nix
+++ common.nix
@@ -10,99 +10,60 @@
       <home-manager/nixos>
     ];
 
-  # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-
-  # tood set hostname for each host
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
-  networking.networkmanager.enable = true;
-
-  # Set your time zone.
-  time.timeZone = "Europe/Stockholm";
-
-  # octoprint
- # services.octoprint.enable = true;
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "sv_SE.UTF-8";
-    LC_IDENTIFICATION = "sv_SE.UTF-8";
-    LC_MEASUREMENT = "sv_SE.UTF-8";
-    LC_MONETARY = "sv_SE.UTF-8";
-    LC_NAME = "sv_SE.UTF-8";
-    LC_NUMERIC = "sv_SE.UTF-8";
-    LC_PAPER = "sv_SE.UTF-8";
-    LC_TELEPHONE = "sv_SE.UTF-8";
-    LC_TIME = "sv_SE.UTF-8";
-  };
-
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "se";
-    variant = "us";
-  };
-
-  # Configure console keymap
-  console.keyMap = "us";
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.jonas = {
-    isNormalUser = true;
-    description = "jonas";
-    extraGroups = [ "networkmanager" "wheel" "dialout" ];
-    packages = with pkgs; [];
-    shell = pkgs.zsh;
-  };
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  # fonts
-  fonts.packages = with pkgs; [
-	nerdfonts
-	noto-fonts
-  	noto-fonts-cjk-sans
-  	noto-fonts-emoji
-  	liberation_ttf
-  	fira-code
-  	fira-code-symbols
-  	mplus-outline-fonts.githubRelease
-  	dina-font
-  	proggyfonts
-  ];
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
-	btop-rocm
 	blender
+	btop-rocm
 	discord
+	ffmpeg
 	firefox
 	godot_4
+	gh
+	kdePackages.dolphin
 	keepassxc
+	rnote
+	pavucontrol
+	podman-tui
+	nautilus
 	neovim
 	nextcloud-client
 	ouch
+	vlc
+	vscode
 	wireguard-tools
 	wl-clipboard
+	xournalpp
   ];
+
+  ## docker and podman
+  # docker
+  virtualisation.docker.enable = true;
+  # podman
+  virtualisation.containers.enable = true;
+  virtualisation = {
+  	podman = {
+		enable = true;
+		# Required for containers under podman-compose to be able to talk to each other.
+      		defaultNetwork.settings.dns_enabled = true;
+	};
+  };
+  
 
   programs.git = {
   	enable = true;
 	config = {
+		# should probably be done in home-manager
 		user.email = "jonas.bonnaudet@gmail.com";
 		user.name = "Jonas";
+		pull.rebase = true;
 	};
   };
 
@@ -125,7 +86,9 @@
 
     shellAliases = {
       ll = "ls -l";
-      update = "sudo nixos-rebuild switch";
+      rebuild = "sudo nixos-rebuild switch";
+      update = "sudo nix-channel --update";
+      ssh = "TERM=xterm ssh";
     };
     ohMyZsh = {
         enable = true;
@@ -161,17 +124,29 @@
     };
   };
 
+  # enable bluetooth
+  services.blueman.enable = true; # blueman to connect bluetooth devices
+  hardware.bluetooth.enable = true; # enables support for Bluetooth
+  hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
+
+  # fonts
+  fonts.packages = with pkgs; [
+	nerd-fonts.jetbrains-mono
+	noto-fonts
+  	noto-fonts-cjk-sans
+  	noto-fonts-emoji
+  	liberation_ttf
+  	fira-code
+  	fira-code-symbols
+  	mplus-outline-fonts.githubRelease
+  	dina-font
+  	proggyfonts
+  ];
 
   # home manager for config files and stuff lol
   home-manager.users.jonas = {
     /* The home.stateVersion option does not have a default and must be set */
     home.stateVersion = "24.11";
-
-    # wayland.windowManager.sway = {
-    #   enable = true;
-    #   config = rec {
-    # };
-
 
     xdg.configFile = {
     	"sway/config".source = ./dotfiles/sway/config;
@@ -179,6 +154,72 @@
 	"waybar/style.css".source = ./dotfiles/waybar/style.css;
 	"wofi/style.css".source = ./dotfiles/wofi/style.css;
     };
+    programs.ssh = {
+      enable = true;
+      extraConfig = builtins.readFile ./dotfiles/ssh/config;
+    };
+  };
+
+  # auto updates
+#  system.autoUpgrade = {
+#    enable = true;
+#    #flake = inputs.self.outPath;
+#    flags = [
+#      "--update-input"
+#      "nixpkgs"
+#      "-L" # print build logs
+#    ];
+#    dates = "10:00";
+#  };
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # tood set hostname for each host
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Europe/Stockholm";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "sv_SE.UTF-8";
+    LC_IDENTIFICATION = "sv_SE.UTF-8";
+    LC_MEASUREMENT = "sv_SE.UTF-8";
+    LC_MONETARY = "sv_SE.UTF-8";
+    LC_NAME = "sv_SE.UTF-8";
+    LC_NUMERIC = "sv_SE.UTF-8";
+    LC_PAPER = "sv_SE.UTF-8";
+    LC_TELEPHONE = "sv_SE.UTF-8";
+    LC_TIME = "sv_SE.UTF-8";
+  };
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "se";
+    variant = "us";
+  };
+
+  # Configure console keymap
+  console.keyMap = "us";
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.jonas = {
+    isNormalUser = true;
+    description = "jonas";
+    extraGroups = [ "networkmanager" "wheel" "dialout" "docker" ];
+    packages = with pkgs; [];
+    shell = pkgs.zsh;
   };
 
 
