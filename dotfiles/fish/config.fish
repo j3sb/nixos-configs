d7ed859363d8189f967c3c268378909cf8b6bc7e
jonas <jonas.bonnaudet@gmail.com>
2026-02-03 10:45:01 +0100

    fish, screenshots and more

--- dotfiles/fish/config.fish
+++ dotfiles/fish/config.fish
@@ -6,8 +6,21 @@ source /usr/share/cachyos-fish-config/cachyos-config.fish
 #    # smth smth
 #end
 
+#aliases
+# trash-put
 alias rm "echo use tp"
 alias tp "trash-put"
 
+# git
 alias gst "git status"
+alias gs "git status"
+alias ga "git add"
+alias gp "git push"
+alias gd "git diff"
+function gc
+	git commit -m "$argv"
+end
+
+# ssh
+alias ssh "kitty +kitten ssh"
 
